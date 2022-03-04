a17f0ab363188ce5010bdd04bb37f67113f8a825
JanZachmann <jan.zachmann@conplement.de>
2022-03-04 12:21:19 +0100

    a proposal

--- src/direct_methods.rs
+++ src/direct_methods.rs
@@ -1,18 +1,13 @@
-use crate::{Message};
+use crate::Message;
 use azure_iot_sdk::client::*;
 use log::debug;
-use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
-
-#[derive(Deserialize, Debug, Serialize)]
-struct Factory {
-    reset: String,
-}
 
 pub fn get_direct_methods(
     _tx_app2client: Arc<Mutex<Sender<Message>>>,
@@ -27,35 +22,16 @@ pub fn get_direct_methods(
 pub fn reset_to_factory_settings(
     in_json: serde_json::Value,
 ) -> Result<Option<serde_json::Value>, Box<dyn Error + Send + Sync>> {
-    let content: Factory;
-    let result = match serde_json::from_value(in_json) {
-        Ok(param) => {
-            content = param;
-            debug!("direct method called with: {:?}", content.reset);
-            write_to_file(content.reset)
+    match &in_json["reset"].as_str() {
+        Some(reset_type) => {
+            debug!("direct method called with: {:?}", reset_type);
+            OpenOptions::new()
+                .write(true)
+                .create(false)
+                .open("/run/factory-reset/systemd-trigger")?
+                .write(reset_type.as_bytes())?;
+            Ok(None)
         }
-        _ => {
-            "param not supported".to_string()
-        }
-    };
-
-    Ok(Some(serde_json::to_value(result).unwrap()))
-}
-
-fn write_to_file(reset_type: String) -> String {
-    match OpenOptions::new()
-        .write(true)
-        .create(false)
-        .open("/run/factory-reset/systemd-trigger")
-    {
-        Ok(mut file) => {
-            let content = format!("{}\n", reset_type.as_str());
-
-            file.write(content.as_bytes()).unwrap();
-            "Ok".to_string()
-        }
-        _ => {
-            "file not exists".to_string()
-        }
+        _ => Ok(Some(json!("param not supported"))),
     }
 }
--- src/twin.rs
+++ src/twin.rs
@@ -1,23 +1,13 @@
 use crate::Message;
 use azure_iot_sdk::client::*;
-use time::{OffsetDateTime};
-use time::format_description::well_known::Rfc3339;
-use log::{debug, warn, error};
-use serde::{Deserialize, Serialize};
+use log::warn;
+use serde_json::json;
+use std::error::Error;
 use std::process::Command;
 use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Status {
-    status: String,
-    date: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct FactoryResetStatus {
-    factory_reset_status: Status,
-}
+use time::format_description::well_known::Rfc3339;
+use time::OffsetDateTime;
 
 pub fn update(
     _state: TwinUpdateState,
@@ -27,56 +17,40 @@ pub fn update(
     warn!("Received unexpected desired twin properties");
 }
 
-pub fn report_factory_reset_result(tx_app2client: Arc<Mutex<Sender<Message>>>) {
+pub fn report_factory_reset_result(
+    tx_app2client: Arc<Mutex<Sender<Message>>>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
     let output = Command::new("sh")
         .arg("-c")
         .arg("fw_printenv factory-reset-status")
-        .output()
-        .expect("failed to execute process");
+        .output()?;
 
-    let get_env = String::from_utf8(output.stdout).unwrap();
-    let split = get_env.split("=");
-    let vec: Vec<&str> = split.collect();
+    let status = String::from_utf8(output.stdout)?;
+    let vec: Vec<&str> = status.split("=").collect();
 
-    if vec.len() == 2 {
-        let reset_state: String;
-        match vec[1] {
-            "0:0\n" => {
-                debug!("successfully");
-                reset_state = "successfully".to_string();
+    let status = match vec[..] {
+        [_, s] => match s {
+            "0:0\n" => Ok("successfully"),
+            "1:-\n" => Err("Unexpected factory reset type in result"),
+            _ => Err("Unexpected factory reset result"),
+        },
+        _ => Err("Unexpected factory reset result format"),
+    };
+
+    tx_app2client
+        .lock()
+        .unwrap()
+        .send(Message::Reported(json!({
+            "FactoryResetStatus": {
+                "status": status.unwrap_or_else(|e| e),
+                "date": OffsetDateTime::now_utc().format(&Rfc3339)?.to_string(),
             }
-            "1:-\n" => {
-                error!("Unexpected factory reset type in result");
-                reset_state = "type not supported".to_string();
-            }
-            _ => {
-                error!("error");
-                reset_state = "error".to_string();
-            }
-        }
+        })))?;
 
-        let state = FactoryResetStatus {
-            factory_reset_status: Status {
-                status: reset_state,
-                date: OffsetDateTime::now_utc().format(&Rfc3339).unwrap().to_string(),
-            },
-        };
-        let serialized_state = serde_json::to_string(&state).unwrap();
-        let map: serde_json::Map<String, serde_json::Value> =
-            serde_json::from_str(&serialized_state).unwrap();
+    Command::new("sh")
+        .arg("-c")
+        .arg("fw_setenv factory-reset-status")
+        .output()?;
 
-        debug!("Report factory reset: {:?}", map);
-
-        tx_app2client
-            .lock()
-            .unwrap()
-            .send(Message::Reported(serde_json::Value::Object(map)))
-            .unwrap();
-
-        Command::new("sh")
-            .arg("-c")
-            .arg("fw_setenv factory-reset-status")
-            .output()
-            .expect("failed to execute process");
-    }
+    Ok(())
 }
