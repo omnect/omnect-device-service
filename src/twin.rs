b2af8a531b0a30500d73cab4eddf17da8ce1abc9
Joerg Zeidler <joerg.zeidler@conplement.de>
2022-03-08 08:40:20 +0100

    styling

--- src/twin.rs
+++ src/twin.rs
@@ -1,6 +1,6 @@
 use crate::Message;
 use azure_iot_sdk::{client::*, IotError};
-use log::{warn, debug};
+use log::{debug, warn};
 use serde_json::json;
 use std::process::Command;
 use std::sync::mpsc::Sender;
@@ -28,35 +28,35 @@ pub fn report_factory_reset_result(
     let vec: Vec<&str> = status.split("=").collect();
 
     let status = match vec[..] {
-        ["factory-reset-status", "0:0\n"] => Ok(("succeeded",true)),
-        ["factory-reset-status", "1:-\n"] => Ok(("unexpected factory reset type",true)),
-        ["factory-reset-status", "\n"] => Ok(("normal boot without factory reset",false)),
-        ["factory-reset-status", _] => Ok(("failed",true)),
-        _ => Err(("unexpected factory reset result format",false)),
+        ["factory-reset-status", "0:0\n"] => Ok(("succeeded", true)),
+        ["factory-reset-status", "1:-\n"] => Ok(("unexpected factory reset type", true)),
+        ["factory-reset-status", "\n"] => Ok(("normal boot without factory reset", false)),
+        ["factory-reset-status", _] => Ok(("failed", true)),
+        _ => Err(("unexpected factory reset result format", false)),
     };
 
     match status {
-        Ok((update_twin,true)) => {
+        Ok((update_twin, true)) => {
             tx_app2client
-            .lock()
-            .unwrap()
-            .send(Message::Reported(json!({
-                "factory_reset_status": {
-                    "status": update_twin,
-                    "date": OffsetDateTime::now_utc().format(&Rfc3339)?.to_string(),
-                }
-            })))?;
+                .lock()
+                .unwrap()
+                .send(Message::Reported(json!({
+                    "factory_reset_status": {
+                        "status": update_twin,
+                        "date": OffsetDateTime::now_utc().format(&Rfc3339)?.to_string(),
+                    }
+                })))?;
             Command::new("sh")
-            .arg("-c")
-            .arg("fw_setenv factory-reset-status")
-            .output()?;
-        },
-        Ok((update_twin,false)) => {
-            debug!("{}",update_twin);
-        },
-        Err((update_twin,_)) => {
-            warn!("factory reset result: {}",update_twin);
-        },
+                .arg("-c")
+                .arg("fw_setenv factory-reset-status")
+                .output()?;
+        }
+        Ok((update_twin, false)) => {
+            debug!("{}", update_twin);
+        }
+        Err((update_twin, _)) => {
+            warn!("factory reset result: {}", update_twin);
+        }
     };
 
     Ok(())
