4509bf21c31914071fbdc69277b8424a974ac861
SteffenRumler <steffen.rumler@conplement.de>
2022-08-24 10:55:46 +0200

    xxxxxxxxxxxxxxxxxxx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use env_logger::{Builder, Env};
-use log::{debug, error};
+use log::{info, error};
 use std::process;
 
 fn main() {
-    debug!("module version: {}", env!("CARGO_PKG_VERSION"));
     if cfg!(debug_assertions) {
         Builder::from_env(Env::default().default_filter_or("debug")).init();
     } else {
         Builder::from_env(Env::default().default_filter_or("info")).init();
     }
+    info!("module version: {}", env!("CARGO_PKG_VERSION"));
     if let Err(e) = demo_portal_module::run() {
         error!("Application error: {}", e);
 
