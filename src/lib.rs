be54580563de0f57206c1bf7be51fc575c0242c9
JanZachmann <jan.zachmann@conplement.de>
2023-02-24 17:42:53 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -71,8 +71,7 @@ pub async fn run() -> Result<()> {
         .watch(Path::new(&history_consent_path), RecursiveMode::Recursive)
         .context("debouncer history_consent_path")?;
 
-    //client.run(None, methods, tx_client2app, rx_app2client);
-    client.run(Some("HostName=omnect-cp-dev-iot-hub.azure-devices.net;DeviceId=jza-02:42:ac:11:00:02;ModuleId=omnect-device-service;SharedAccessKey=6rd7wSR2ASK6x6EfTUNaqhYU4TzJ63TZyjBW32DWgwI="), methods, tx_client2app, rx_app2client);
+    client.run(None, methods, tx_client2app, rx_app2client);
 
     loop {
         match rx_client2app.recv_timeout(Duration::from_secs(RX_CLIENT2APP_TIMEOUT)) {
