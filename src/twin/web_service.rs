678e1e288bd47cffebcbfa5f425f515b41e0ca07
Joerg Zeidler <62105035+JoergZeidler@users.noreply.github.com>
2024-04-12 08:29:37 +0200

    added cors handling

--- src/twin/web_service.rs
+++ src/twin/web_service.rs
@@ -1,5 +1,6 @@
+use actix_cors::Cors;
 use actix_server::ServerHandle;
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{http, web, App, HttpResponse, HttpServer, Responder};
 use tokio::sync::{mpsc, oneshot};
 
 #[derive(Debug)]
@@ -17,12 +18,20 @@ pub struct WebService {
 impl WebService {
     pub fn new(tx_request: mpsc::Sender<Command>) -> Self {
         let srv = HttpServer::new(move || {
+            let cors = Cors::default()
+                .allowed_origin(format!("http://{}:{}", "127.0.0.1", "8085").as_str())
+                .allowed_methods(vec!["GET", "POST"])
+                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                .allowed_header(http::header::CONTENT_TYPE)
+                .max_age(3600);
+
             App::new()
+                .wrap(cors)
                 .app_data(web::Data::new(tx_request.clone()))
                 .route("/reboot", web::put().to(Self::reboot))
                 .route("/os-version", web::get().to(Self::os_version))
         })
-        .bind("localhost:12347")
+        .bind("localhost:8086")
         .unwrap()
         .run();
         let srv_handle = srv.handle();
@@ -31,9 +40,7 @@ impl WebService {
 
         /*     let response = reqwest::get("http://localhost:12347").await.unwrap();
         println!("Response code: {:?}", response.status()); */
-        WebService {
-            srv_handle,
-        }
+        WebService { srv_handle }
     }
 
     pub async fn shutdown(&self) {
@@ -55,6 +62,8 @@ impl WebService {
 
         tx_request.send(cmd).await.unwrap();
 
-        rx_reply.await.unwrap().to_string()
+        HttpResponse::Ok()
+            .content_type("application/json")
+            .body(rx_reply.await.unwrap().to_string())
     }
 }
