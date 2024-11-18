726da8f91db23fdc326af200affafbc5a24635e0
Joerg Zeidler <62105035+JoergZeidler@users.noreply.github.com>
2024-11-18 16:00:28 +0100

    chore: added metrics support CPU,Temp and Memory
    Signed-off-by: Joerg Zeidler <62105035+JoergZeidler@users.noreply.github.com>

--- src/twin/system_info.rs
+++ src/twin/system_info.rs
@@ -5,12 +5,59 @@ use anyhow::{bail, Context, Result};
 use async_trait::async_trait;
 use azure_iot_sdk::client::{IotHubClient, IotMessage};
 use lazy_static::lazy_static;
-use log::{debug, warn};
+use log::{debug, error, info, warn};
 use serde::Serialize;
 use serde_json::json;
 use std::env;
 use std::path::Path;
+use sysinfo::{Components, MemoryRefreshKind};
+use sysinfo::{CpuRefreshKind, RefreshKind, System};
+use time::format_description::well_known::Rfc3339;
 use tokio::sync::mpsc;
+use tokio::time::Duration;
+
+lazy_static! {
+    static ref DEVICE_ID: String =
+        std::env::var("IOTEDGE_DEVICEID").unwrap_or("deviceId".to_string());
+    static ref IOTHUB: String =
+        std::env::var("IOTEDGE_IOTHUBHOSTNAME").unwrap_or("iothubHostName".to_string());
+    static ref MODULE_ID: String =
+        std::env::var("IOTEDGE_MODULEID").unwrap_or("omnect-device-service".to_string());
+}
+
+#[derive(Serialize)]
+struct Label {
+    edge_device: String,
+    iothub: String,
+    module_name: String,
+}
+
+#[derive(Serialize)]
+struct Metric {
+    #[serde(rename = "TimeGeneratedUtc")]
+    time_generated_utc: String,
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Value")]
+    value: f64,
+    #[serde(rename = "Labels")]
+    labels: Label,
+}
+
+impl Metric {
+    fn new(time: String, name: &str, value: f64) -> Metric {
+        Metric {
+            time_generated_utc: time,
+            name: name.to_string(),
+            value,
+            labels: Label {
+                edge_device: DEVICE_ID.to_string(),
+                iothub: IOTHUB.to_string(),
+                module_name: MODULE_ID.to_string(),
+            },
+        }
+    }
+}
 
 lazy_static! {
     static ref TIMESYNC_FILE: &'static Path = if cfg!(feature = "mock") {
@@ -47,8 +94,11 @@ impl Feature for SystemInfo {
     async fn connect_twin(
         &mut self,
         tx_reported_properties: mpsc::Sender<serde_json::Value>,
-        _tx_outgoing_message: mpsc::Sender<IotMessage>,
+        tx_outgoing_message: mpsc::Sender<IotMessage>,
     ) -> Result<()> {
+        Some(tokio::spawn(SystemInfo::data_collector(
+            tx_outgoing_message.clone(),
+        )));
         self.tx_reported_properties = Some(tx_reported_properties);
         self.report().await
     }
@@ -117,5 +167,74 @@ impl SystemInfo {
         }))
         .await
         .context("report: send")
+    }
+
+    async fn data_collector(tx_outgoing_message: mpsc::Sender<IotMessage>) {
+        // configure interval of wind speed and wind direction samples in seconds
+        let mut collect_interval = tokio::time::interval(Duration::from_secs(60));
+
+        let mut components = Components::new_with_refreshed_list();
+
+        let mut s = System::new_with_specifics(
+            RefreshKind::new()
+                .with_cpu(CpuRefreshKind::everything())
+                .with_memory(MemoryRefreshKind::everything()),
+        );
+
+        info!("data collector!");
+
+        loop {
+            collect_interval.tick().await;
+
+            let time = match time::OffsetDateTime::now_utc().format(&Rfc3339) {
+                Ok(time) => time,
+                Err(e) => {
+                    error!("timestamp could not be generated: {e}");
+                    continue;
+                }
+            };
+            components.refresh_list();
+            s.refresh_cpu_usage();
+            s.refresh_memory();
+
+            // for component in components.iter() {
+            //     debug!("{} {}°C", component.label(), component.temperature());
+            // }
+            let first_component = components.iter().next().unwrap();
+            debug!(
+                "{} {}°C",
+                first_component.label(),
+                first_component.temperature()
+            );
+
+            let metric_list = vec![
+                Metric::new(
+                    time.clone(),
+                    "temperature",
+                    first_component.temperature() as f64,
+                ),
+                Metric::new(time.clone(), "cpu_usage", s.global_cpu_usage() as f64),
+                Metric::new(time.clone(), "ram_used", s.used_memory() as f64),
+                Metric::new(time.clone(), "ram_available", s.available_memory() as f64),
+            ];
+
+            match serde_json::to_vec(&metric_list) {
+                Ok(json) => {
+                    match IotMessage::builder()
+                        .set_body(json)
+                        .set_content_type("application/json")
+                        .set_content_encoding("utf-8")
+                        .set_output_queue("metrics")
+                        .build()
+                    {
+                        Ok(msg) => {
+                            let _ = tx_outgoing_message.send(msg).await;
+                        }
+                        Err(e) => error!("telemetry message could not be transmitted: {e}"),
+                    }
+                }
+                Err(e) => error!("metrics list could not be converted to vector: {e}"),
+            }
+        }
     }
 }
