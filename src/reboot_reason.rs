c0d94387839ac01e3aa607cf030432094cc08583
Harry Waschkeit <44188360+HarryWaschkeit@users.noreply.github.com>
2025-01-31 12:37:01 +0100

    fix(omnect-device-service): make reboot reason arguments loggin shell safe
    Signed-off-by: Harry Waschkeit <44188360+HarryWaschkeit@users.noreply.github.com>

--- src/reboot_reason.rs
+++ src/reboot_reason.rs
@@ -6,9 +6,12 @@ use std::process::Command;
 static REBOOT_REASON_SCRIPT: &str = "/usr/sbin/omnect_log_reboot_reason_to_pmsg.sh";
 
 pub fn reboot_reason(reason: &str, extra_info: &str) -> Result<()> {
+    // make arguments shell script proof
+    let reason = format!("\"{reason_str}\"", reason_str = reason.replace("\"", "'"));
+    let extra_info = format!("\"{extra_info_str}\"", extra_info_str = extra_info.replace("\"", "'"));
     ensure!(
         Command::new(REBOOT_REASON_SCRIPT)
-            .args([reason, extra_info])
+            .args([reason.clone(), extra_info.clone()])
             .status()
             .context("failed to invoke '{REBOOT_REASON_SCRIPT} {reason} \"{extra_info}\"'")?
             .success(),
