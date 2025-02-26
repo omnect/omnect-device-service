e63d48820cc5eace838908cd4f01031288ffe3b6
Harry Waschkeit <44188360+HarryWaschkeit@users.noreply.github.com>
2025-02-26 14:32:59 +0100

    fix: reboot reason script needs root permissions for EFI var access
    Signed-off-by: Harry Waschkeit <44188360+HarryWaschkeit@users.noreply.github.com>

--- src/reboot_reason.rs
+++ src/reboot_reason.rs
@@ -11,8 +11,8 @@ pub fn reboot_reason(reason: &str, extra_info: &str) -> Result<()> {
     let reason = format!("{reason_str}", reason_str = reason.replace("\"", "'"));
     let extra_info = format!("{extra_info_str}", extra_info_str = extra_info.replace("\"", "'"));
     ensure!(
-        Command::new(REBOOT_REASON_SCRIPT)
-            .args([cmd, &reason.clone(), &extra_info.clone()])
+        Command::new("sudo")
+            .args([REBOOT_REASON_SCRIPT, cmd, &reason.clone(), &extra_info.clone()])
             .status()
             .context("failed to invoke '{REBOOT_REASON_SCRIPT} {reason} \"{extra_info}\"'")?
             .success(),
