ea5cb3d236959ca03aacb63b57ab16ed4d4f26c4
Joerg Zeidler <62105035+JoergZeidler@users.noreply.github.com>
2025-04-24 16:03:55 +0200

    refactoring
    Signed-off-by: Joerg Zeidler <62105035+JoergZeidler@users.noreply.github.com>

--- src/web_service.rs
+++ src/web_service.rs
@@ -7,13 +7,11 @@ use log::{debug, error, info, warn};
 use reqwest::{header, Client};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::{collections::HashMap, fs::OpenOptions, str::FromStr, sync::LazyLock};
+use std::{fs::OpenOptions, str::FromStr, sync::LazyLock};
 use tokio::sync::{mpsc, oneshot, Mutex};
 
 static PUBLISH_CHANNEL_MAP: LazyLock<Mutex<serde_json::Map<String, serde_json::Value>>> =
     LazyLock::new(|| Mutex::new(serde_json::Map::default()));
-static PUBLISH_ENDPOINTS: LazyLock<Mutex<HashMap<String, PublishEndpoint>>> =
-    LazyLock::new(|| Mutex::new(HashMap::new()));
 
 macro_rules! centrifugo_endpoints_path {
     () => {{
@@ -159,38 +157,22 @@ impl WebService {
         match serde_json::from_slice::<PublishEndpointRequest>(&body) {
             Ok(request) => {
                 // Read the existing JSON file, or create an empty array if it doesn't exist
-                let mut endpoints: Vec<PublishEndpointRequest> =
-                    match std::fs::read_to_string(centrifugo_endpoints_path!()) {
-                        Ok(content) => {
-                            serde_json::from_str(&content).unwrap_or_else(|_| Vec::new())
-                        }
-                        Err(_) => Vec::new(),
-                    };
+                let mut endpoints: Vec<PublishEndpointRequest> = read_centrifugo_endpoints();
+
                 // Check if the id exists, update if found, else push new
-                let mut found = false;
-                for endpoint in endpoints.iter_mut() {
-                    if endpoint.id == request.id {
-                        *endpoint = request.clone();
-                        found = true;
-                        break;
-                    }
-                }
-                if !found {
+                if let Some(existing) = endpoints.iter_mut().find(|ep| ep.id == request.id) {
+                    *existing = request.clone();
+                } else {
                     endpoints.push(request);
                 }
 
-                let _ = serde_json::to_writer_pretty(
-                    OpenOptions::new()
-                        .create(true)
-                        .write(true)
-                        .truncate(true)
-                        .open(centrifugo_endpoints_path!())
-                        .unwrap(),
-                    &endpoints,
-                )
-                .context("web_service: serde_json::to_writer_pretty");
-
-                HttpResponse::Ok().finish()
+                match write_centrifiguo_endpoints(&endpoints) {
+                    Ok(_) => HttpResponse::Ok().finish(),
+                    Err(e) => {
+                        error!("write_centrifiguo_endpoints: {e:#}");
+                        HttpResponse::BadRequest().body(e.to_string())
+                    }
+                }
             }
             Err(e) => {
                 error!("couldn't parse PublishEndpointRequest from body: {e:#}");
@@ -205,25 +187,16 @@ impl WebService {
     ) -> HttpResponse {
         debug!("WebService delete_publish_endpoint");
 
-        let mut endpoints: Vec<PublishEndpointRequest> =
-            match std::fs::read_to_string(centrifugo_endpoints_path!()) {
-                Ok(content) => serde_json::from_str(&content).unwrap_or_else(|_| Vec::new()),
-                Err(_) => Vec::new(),
-            };
-        endpoints.retain(|client| client.id != path.id);
+        let mut endpoints: Vec<PublishEndpointRequest> = read_centrifugo_endpoints();
+        endpoints.retain(|endpoint_info| endpoint_info.id != path.id);
 
-        let _ = serde_json::to_writer_pretty(
-            OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(centrifugo_endpoints_path!())
-                .unwrap(),
-            &endpoints,
-        )
-        .context("web_service: serde_json::to_writer_pretty");
-
-        HttpResponse::Ok().finish()
+        match write_centrifiguo_endpoints(&endpoints) {
+            Ok(_) => HttpResponse::Ok().finish(),
+            Err(e) => {
+                error!("write_centrifiguo_endpoints: {e:#}");
+                HttpResponse::BadRequest().body(e.to_string())
+            }
+        }
     }
 
     async fn factory_reset(
@@ -398,18 +371,14 @@ pub async fn publish(channel: PublishChannel, value: serde_json::Value) {
 async fn publish_to_endpoints(msg: serde_json::Value) -> Result<()> {
     debug!("publish to endpoints");
 
-    let endpoints: Vec<PublishEndpointRequest> =
-        match std::fs::read_to_string(centrifugo_endpoints_path!()) {
-            Ok(content) => serde_json::from_str(&content).unwrap_or_else(|_| Vec::new()),
-            Err(_) => Vec::new(),
-        };
+    let endpoints: Vec<PublishEndpointRequest> = read_centrifugo_endpoints();
 
     let mut errors = Vec::new();
 
-    for endpoint in endpoints {
+    for endpoint_info in endpoints {
         let mut headers = header::HeaderMap::new();
 
-        for h in &endpoint.endpoint.headers {
+        for h in &endpoint_info.endpoint.headers {
             headers.insert(
                 header::HeaderName::from_str(&h.name)?,
                 header::HeaderValue::from_str(&h.value)?,
@@ -423,43 +392,54 @@ async fn publish_to_endpoints(msg: serde_json::Value) -> Result<()> {
             .danger_accept_invalid_certs(true)
             .build()
         else {
-            // bail!(
-            //     "publish_to_endpoints: building request {msg} to {} failed",
-            //     endpoint.endpoint.url
-            // );
             errors.push(format!(
                 "publish_to_endpoints: building request {msg} to {} failed",
-                endpoint.endpoint.url
+                endpoint_info.endpoint.url
             ));
             continue;
         };
         if let Err(e) = request
-            .post(&endpoint.endpoint.url)
+            .post(&endpoint_info.endpoint.url)
             .body(msg.to_string())
             .send()
             .await
         {
-            //     bail!(
-            //     "publish_to_endpoints: sending {msg} to {} (failed with {e}). Endpoint not present?",
-            //     endpoint.endpoint.url
-            // );
             errors.push(format!("publish_to_endpoints: sending {msg} to {} (failed with {e}). Endpoint not present?",
-            endpoint.endpoint.url));
+            endpoint_info.endpoint.url));
             continue;
         }
 
         info!(
             "publish_to_endpoints: successfully sent {msg} to {}.",
-            endpoint.endpoint.url
+            endpoint_info.endpoint.url
         )
     }
     if !errors.is_empty() {
-        // Return the first error, or combine them as needed
-        //return Err(anyhow::anyhow!("Errors occurred: {:?}", errors));
         bail!("Errors occurred: {:?}", errors)
     }
 
     Ok(())
+}
+
+fn write_centrifiguo_endpoints(endpoints: &Vec<PublishEndpointRequest>) -> Result<()> {
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(centrifugo_endpoints_path!())
+        .context("Failed to open endpoints file")?;
+
+    serde_json::to_writer_pretty(file, endpoints)
+        .context("Failed to write endpoints to JSON file")?;
+
+    Ok(())
+}
+
+fn read_centrifugo_endpoints() -> Vec<PublishEndpointRequest> {
+    match std::fs::read_to_string(centrifugo_endpoints_path!()) {
+        Ok(content) => serde_json::from_str(&content).unwrap_or_else(|_| Vec::new()),
+        Err(_) => Vec::new(),
+    }
 }
 
 #[cfg(test)]
