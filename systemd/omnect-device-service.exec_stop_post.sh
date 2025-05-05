90a873d547af717cd52febf3675d72c9cac87150
Harry Waschkeit <44188360+HarryWaschkeit@users.noreply.github.com>
2025-05-05 14:45:22 +0200

    fix(reboot-reason): added reason logging for reboots triggered in exec-stop-post script (#153)
    Signed-off-by: Harry Waschkeit <44188360+HarryWaschkeit@users.noreply.github.com>

--- systemd/omnect-device-service.exec_stop_post.sh
+++ systemd/omnect-device-service.exec_stop_post.sh
@@ -9,6 +9,7 @@ echo SERVICE_RESULT=${SERVICE_RESULT}, EXIT_CODE=${EXIT_CODE}, EXIT_STATUS=${EXI
 
 function reboot() {
   echo "reboot triggered by ${script}: ${1}"
+  /usr/sbin/omnect_reboot_reason.sh log swupdate-validation-failed "${1}"
   dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Reboot" boolean:true
 }
 
